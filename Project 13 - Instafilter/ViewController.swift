e5b5bed34814c8bdf41b41fa8b733ae468623bd8
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-21 10:16:03 +0100

    FEAT: add UI storyboard elements, constraints, outlets and actions.

--- Project 13 - Instafilter/ViewController.swift
+++ Project 13 - Instafilter/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChange(_ sender: Any) {
+    }
+    
+    
 }
 
